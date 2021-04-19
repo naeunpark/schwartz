9c74a58bf232351f531f223f6d6529c04ae79461
naeun park <paknaun@gmail.com>
2021-04-19 16:49:11 +1200

    first frame work

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import del from "del";
+import gulp from "gulp";
+import sass from "gulp-sass";
+import minify from "gulp-csso";
+import autoprefixer from "gulp-autoprefixer";
+
+sass.compiler = require("node-sass");
+
+const routes = {
+    css : {
+        watch : "src/scss/*",
+        src : "src/scss/styles.scss",
+        dist : "dist/css"
+    }
+};
+
+const styles = () =>
+    gulp
+        .src(routes.css.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({
+            flexbox: true,
+            grid: "autoplace"
+        }))
+        .pipe(minify())
+        .pipe(gulp.dest(routes.css.dist));
+
+const watch = () =>
+        gulp.watch(routes.css.watch, styles);
+
+const clean = () =>
+        del([routes.css.dist]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([styles]);
+
+const live = gulp.series([watch]);
+
+export const dev = gulp.series([prepare, assets, live]);
